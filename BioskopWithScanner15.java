a452d13a71a662b056846ab70659a6b3d54812b7
martaaa-16 <martaprama@gmail.com>
2024-11-07 10:10:18 +0700

    hi!

--- BioskopWithScanner15.java
+++ BioskopWithScanner15.java
@@ -23,32 +23,41 @@ public class BioskopWithScanner15 {
                 case 1:
                     System.out.print("Masukkan nama: ");
                     nama = sc.nextLine();
-                    System.out.print("Masukkan baris (1-4): ");
+
+                    while (true) {
+                        System.out.print("Masukkan baris (1-4): ");
                     baris = sc.nextInt();
                     System.out.print("Masukkan kolom (1-2): ");
                     kolom = sc.nextInt();
                     sc.nextLine();
-                
-                    if (baris > 0 && baris <= 4 && kolom > 0 && kolom <= 2) {
-                        penonton[baris - 1][kolom - 1] = nama;
-                        System.out.println("Data berhasil disimpan.");
-                    } else {
-                        System.out.println("Posisi baris atau kolom tidak tersedia.");
+
+                        if (baris > 0 && baris <= 4 && kolom > 0 && kolom <= 2) {
+                            if (penonton[baris - 1][kolom - 1] == null) {
+                                penonton[baris - 1][kolom - 1] = nama;
+                                System.out.println("Data berhasil disimpan.");
+                                break;
+                            } else {
+                                System.out.println("Kursi pada baris " + baris + " kolom " + kolom + " sudah terisi oleh " + penonton[baris - 1][kolom - 1] + ".");
+                                System.out.println("Silahkan pilih kursi lain.");
+                            }
+                        } else {
+                            System.out.println("Posisi baris atau kolom tidak valid. Silahkan coba lagi!");
+                        } 
                     }
                     break;
-                    
+
                 case 2:
-                System.out.println("=== Dafatar Penonton ===");
-                for (int i = 0; i < penonton.length; i++) {
-                    for (int j = 0; j < penonton[i].length; j++) {
-                        if (penonton[i][j] != null) {
-                            System.out.println("Baris " + (i + 1) + " Kolom " + (j + 1) + ": " + penonton[i][j]);
-                        } else {
-                            System.out.println("Baris " + (i + 1) + " Kolom " + (j + 1) + ": [Kosong]");
+                    System.out.println("=== Daftar Penonton ===");
+                    for (int i = 0; i < penonton.length; i++) {
+                        for (int j = 0; j < penonton[i].length; j++) {
+                            if (penonton[i][j] != null) {
+                                System.out.println("Baris " + (i + 1) + " Kolom " + (j + 1) + ": " + penonton[i][j]);
+                            } else {
+                                System.out.println("Baris " + (i + 1) + " Kolom " + (j + 1) + ": [Kosong]");
+                            }
                         }
                     }
-                }
-                break;
+                    break;
 
                 case 3:
                     System.out.println("Terima kasih telah menggunakan aplikasi ini.");
@@ -56,9 +65,10 @@ public class BioskopWithScanner15 {
                     return;
 
                 default:
-                System.out.println("Menu tidak valid. Silahkan pilih menu antara 1-3.");    
-                break;
+                    System.out.println("Menu tidak valid. Silahkan pilih menu antara 1-3.");    
+                    break;
+                }
             }
-        }
+        
     }
 }
